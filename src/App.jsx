93c9cdce2063cf4275f7267edd92ef4d659ba671
Abdirahem <elskumar.am@gmail.com>
2024-06-26 10:09:04 +0300

    First commit of my project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import { About } from "./pages/About"; // Assuming About.jsx is in the same directory
+import { Contact } from "./pages/Contact"; // Assuming About.jsx is in the same directory
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+export function About() {
+  // ... component logic
+  return (
+    <div>
+      <h1>About Us</h1>
+      {/* ... more content */}
+    </div>
+  );
+}
